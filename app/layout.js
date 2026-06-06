55380ec585264d076fd90f211b737641ac998a6b
dracoon6 <86674590+dracoon6@users.noreply.github.com>
2026-06-05 19:56:30 -0600

    work on this ticket as well as making the db paths

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import SidebarNav from './ui/SidebarNav';
 
 export const metadata = {
   title: 'Handcrafted Haven',
@@ -8,7 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex bg-gray-50">
+        <SidebarNav />
+        <div className="flex-1 ml-[365px]">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
